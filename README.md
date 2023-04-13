72105819cdfb548a96517e281b3f6c9cc08e4924
anojkb <35885255+anojkb@users.noreply.github.com>
2023-04-13 10:26:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mjsayp-hbyt6p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mjsayp-hbyt6p)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  productsNew = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
